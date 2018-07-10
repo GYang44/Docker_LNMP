ceb91b5c6e2664d85e7553a3e052b7a3b7016e6d
Guojun Yang <gyang44@ouutlook.com>
2018-07-10 17:14:30 -0400

    fixed config reading error in selinux environemt, db to be fixed

---
+++ scriptHost/moveCertificates.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#this meant to be run from the directory from Dockerfile
+DOMAIN='www.guojunyang.net'
+echo --- moving for $DOMAIN ---
+cp /etc/letsencrypt/live/$DOMAIN/chain.pem ./certs/
+cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem ./certs/
+cp /etc/letsencrypt/live/$DOMAIN/privkey.pem ./certs/
+cp /etc/letsencrypt/live/$DOMAIN/cert.pem ./certs/
+chown gyang:gyang ./certs/*
+echo --- finish moving certs for $DOMAIN ---
